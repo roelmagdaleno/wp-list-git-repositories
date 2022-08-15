538fa8c0d54ded5bce99a3f08539e37a87fd23fe
Roel Magdaleno <roelmagdaleno@gmail.com>
2022-08-14 19:38:35 -0500

    Initial project files

---
+++ wp-list-github-repositories.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Plugin Name: GitHub Repositories
+ * Plugin URI:  https://github.com/roelmagdaleno/wp-list-github-repositories
+ * Description: WordPress plugin listing your GitHub public repositories.
+ * Version:     0.1.0
+ * Author:      Roel Magdaleno
+ * Author URI:  https://roelmagdaleno.com
+ */
+
+require 'vendor/autoload.php';
